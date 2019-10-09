ed19c9c820cc18db15edb14d4f27e90c088271b9
aghiles.nessah <aghiles.nessah@uha.fr>
2019-10-09 13:43:00 +0200

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'blog-angular';
+  title = 'Mon blog';
+
+  posts = [
+    {
+      title: 'First Post',
+      content: 'Circa hos dies Lollianus primae lanuginis adulescens, Lampadi filius ex praefecto, exploratius causam Maximino spectante, convictus codicem noxiarum artium nondum per aetatem firmato consilio descripsisse, exulque mittendus, ut sperabatur, patris inpulsu provocavit ad principem, et iussus ad eius comitatum duci, de fumo, ut aiunt, in flammam traditus Phalangio Baeticae consulari cecidit funesti carnificis manu.',
+      loveIts: 0,
+      created_at: new Date()
+    },
+    {
+      title: 'Second Post',
+      content: 'Quapropter a natura mihi videtur potius quam ab indigentia orta amicitia, applicatione magis animi cum quodam sensu amandi quam cogitatione quantum illa res utilitatis esset habitura. Quod quidem quale sit, etiam in bestiis quibusdam animadverti potest, quae ex se natos ita amant ad quoddam tempus et ab eis ita amantur ut facile earum sensus appareat. Quod in homine multo est evidentius, primum ex ea caritate quae est inter natos et parentes, quae dirimi nisi detestabili scelere non potest.',
+      loveIts: 0,
+      created_at: new Date()
+    },
+    {
+      title: 'Third Post',
+      content: 'Altera sententia est, quae definit amicitiam paribus officiis ac voluntatibus. Hoc quidem est nimis exigue et exiliter ad calculos vocare amicitiam, ut par sit ratio acceptorum et datorum. Divitior mihi et affluentior videtur esse vera amicitia nec observare restricte, ne plus reddat quam acceperit; neque enim verendum est, ne quid excidat, aut ne quid in terram defluat, aut ne plus aequo quid in amicitiam congeratur.',
+      loveIts: 0,
+      created_at: new Date()
+    }
+  ];
 }
---
+++ src/app/post-list-item/post-list-item.component.ts
@@ -0,0 +1,27 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-post-list-item',
+  templateUrl: './post-list-item.component.html',
+  styleUrls: ['./post-list-item.component.scss']
+})
+export class PostListItemComponent implements OnInit {
+
+  @Input() postTitle: string;
+  @Input() postContent: string;
+  @Input() postCreatedAt: string;
+  @Input() postLoveIts: number;
+
+  constructor() { }
+
+  onLoveIts() {
+    this.postLoveIts++;
+  }
+
+  onNotLoveIts() {
+    this.postLoveIts--;
+  }
+  ngOnInit() {
+  }
+
+}
